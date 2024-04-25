acf21425beda6f77ebf12345f11da0ef50cf1c4d
darwme <115364940+darwme@users.noreply.github.com>
2024-04-25 11:03:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from utils.db import db
 from services.predio import predio
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION
+from dotenv import load_dotenv
 
 app= Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_CONNECTION
@@ -14,5 +15,7 @@ app.register_blueprint(predio)
 with app.app_context():
     db.create_all
 
+port = int(os.getenv("PORT", default="5000"))
+
 if __name__=="__main__":
-    app.run(host='0.0.0.0', debug=True, port=5000)
+    app.run(host='0.0.0.0', debug=True, port=port)
